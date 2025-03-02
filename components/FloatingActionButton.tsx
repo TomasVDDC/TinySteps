dc080cd0a87d079f96bf011e4467cc1847223c1f
Tomas Valdivieso <tomas@Tomass-MacBook-Air.local>
2025-03-02 19:01:28 +0000

    main components of form done

--- components/FloatingActionButton.tsx
+++ components/FloatingActionButton.tsx
@@ -1,28 +1,30 @@
 import { View } from "react-native";
 import { Text } from "~/components/ui/text";
 import { Button } from "~/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
 import { Plus } from "~/lib/icons/Plus";
 import { Pressable } from "react-native";
-import { Input } from "~/components/ui/input";
-import { useForm, Controller } from 'react-hook-form';
-import DateTimePicker from '@react-native-community/datetimepicker';
-
+import HabitForm from "~/components/HabitForm";
+import { useState } from "react";
 
 export default function FloatingActionButton() {
+  const [open, setOpen] = useState(false);
 
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: {
-      habitName: '',
-      date: new Date()
-    }
-  });
+  const handleSubmit = (data: any) => {
+    const transformedData = {
+      ...data,
+      daysPerWeek: data.daysPerWeek.value 
+    };
+    console.log(transformedData);
+    setOpen(false);
+  };
 
-  const onSubmit = (data: any) => console.log(data);
-
+  const handleCancel = () => {
+    setOpen(false);
+  };
 
   return (   
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Pressable
           className="absolute bottom-32 right-0 rounded-full dark:bg-white bg-yellow-500 p-4 shadow-lg dark:shadow-white">
@@ -35,55 +37,12 @@ export default function FloatingActionButton() {
         <DialogHeader>
           <DialogTitle>Add Habit</DialogTitle>
           <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
         </DialogHeader>
 
-
-        <Controller
-        control={control}
-        rules={{
-         required: true,
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <Input
-            placeholder="Habit Name"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-        name="habitName"
-      />
-      
-              <Controller
-        control={control}
-        rules={{
-         required: true,
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <DateTimePicker
-          testID="dateTimePicker"
-          value={value}
-          mode={"time"}
-          is24Hour={true}
-          // DateTimePicker component's onChange provides both an event and the selectedDate, but React Hook Form expects just the value.
-          onChange={(event, selectedDate) => {
-            const currentDate = selectedDate || value;
-            onChange(currentDate);
-          }}
-        />
-        )}
-        name="date"
-      />
-
-
-        <DialogFooter>
-          <Button variant="outline"> <Text>Cancel</Text> </Button>
-          <Button onPress={handleSubmit(onSubmit)}> <Text>Save</Text> </Button>
-        </DialogFooter>
+        <HabitForm onSubmit={handleSubmit} onCancel={handleCancel} />
       </DialogContent>
     </Dialog>
-  
   );
 }       
---
+++ components/HabitForm.tsx
@@ -0,0 +1,151 @@
+import { Controller, useForm } from 'react-hook-form';
+import { Input } from '~/components/ui/input';
+import { Button } from '~/components/ui/button';
+import { Text } from '~/components/ui/text';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { View, ScrollView } from 'react-native';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select';
+import { ToggleGroup, ToggleGroupItem, ToggleGroupIcon } from '~/components/ui/toggle-group';
+import { Bold } from 'lucide-react-native';
+
+type HabitFormData = {
+  habitName: string;
+  notificationTime: Date;
+  daysPerWeek: string;
+  notificationDays: string[];
+};
+
+type HabitFormProps = {
+  onSubmit: (data: HabitFormData) => void;
+  onCancel: () => void;
+};
+
+export default function HabitForm({ onSubmit, onCancel }: HabitFormProps) {
+  const { control, handleSubmit, formState: { errors } } = useForm<HabitFormData>({
+    defaultValues: {
+      habitName: '',
+      notificationTime: new Date(),
+      daysPerWeek: '',
+      notificationDays: []
+    }
+  });
+
+  return (
+    <>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder="Habit Name"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="habitName"
+      />
+      
+      
+
+      <Text className='text-sm font-bold'>Days Per Week</Text>
+
+      <Controller
+        control={control}
+     
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Select value={value} onValueChange={onChange}>
+      <SelectTrigger className='w-[60px]'>
+        <SelectValue
+          className='text-foreground text-sm native:text-lg'
+        />
+      </SelectTrigger>
+      <SelectContent  className='w-[70px]'>
+      <ScrollView className='max-h-32'>
+        <SelectGroup>
+          <SelectItem label='1' value='1'>1</SelectItem>
+          <SelectItem label='2' value='2'>2</SelectItem>
+          <SelectItem label='3' value='3'>3</SelectItem>
+          <SelectItem label='4' value='4'>4</SelectItem>
+          <SelectItem label='5' value='5'>5</SelectItem>
+          <SelectItem label='6' value='6'>6</SelectItem>
+          <SelectItem label='7' value='7'>7</SelectItem>
+        </SelectGroup>
+        </ScrollView>
+      </SelectContent>
+    </Select> 
+    )}
+    name="daysPerWeek"
+    />
+
+    <Text className='text-sm font-bold'>Notification Time</Text>
+
+<Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={value}
+            mode={"time"}
+            is24Hour={true}
+            onChange={(event, selectedDate) => {
+              const currentDate = selectedDate || value;
+              onChange(currentDate);
+            }}
+          />
+        )}
+        name="notificationTime"
+      />
+        <Text className='text-sm font-bold'>Notification Days</Text>
+
+        <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+
+   
+      <ToggleGroup value={value} onValueChange={onChange} type='multiple'>
+        <ToggleGroupItem value='Monday' size='sm'>
+         <Text className='font-bold'> Mon</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Tuesday' size='sm'>
+          <Text className='font-bold'> Tue</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Wednesday' size='sm'>
+          <Text className='font-bold'> Wed</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Thursday' size='sm'>
+          <Text className='font-bold'> Thu</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Friday' size='sm'>
+          <Text className='font-bold'> Fri</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Saturday' size='sm'>
+          <Text className='font-bold'> Sat</Text>
+        </ToggleGroupItem>
+        <ToggleGroupItem value='Sunday' size='sm'>
+          <Text className='font-bold'> Sun</Text>
+        </ToggleGroupItem>
+      </ToggleGroup>
+   )}
+    name="notificationDays"
+    />
+
+    <View className="flex flex-row justify-end space-x-2">
+    <Button variant="outline" onPress={onCancel}>
+        <Text>Cancel</Text>
+    </Button>
+    <Button onPress={handleSubmit(onSubmit)}>
+        <Text>Save</Text>
+    </Button>
+    </View>
+</>
+  );
+} 
